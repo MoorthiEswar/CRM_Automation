d7042dbcbbafec8b87715ecdaf17f9d7189ac0f7
Dell <Dell@DESKTOP-ILOVP9M>
2023-10-11 16:02:53 +0530

    Eswar commit

---
+++ AutomationCRM/src/com_crm_pom/Pricebookpage.java
@@ -0,0 +1,15 @@
+package com_crm_pom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Pricebookpage {
+	@FindBy(xpath = "//input[@type='button'and @value='New Price Book']")
+	private WebElement newpricebookbutton;
+	@FindBy(xpath = "//input[@name='property(Price Book Name)']")
+	private WebElement pricebooknametext;
+	@FindBy(xpath = "//textarea[@name='property(Description)']")
+	private WebElement pricebookdescription;
+	@FindBy(xpath = "(//input[@name='Button'])[3]")
+    private WebElement savebuttonofpricebook;
+}
