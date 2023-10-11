92bc08f1b1c2ad9a3f8c320dc45606fea97fffe9
Veeresh VK <Veeresh VK@VEER>
2023-10-11 17:02:02 +0530

    commit v

--- AutomationCRM/src/com_crm_pom/Products.java
+++ AutomationCRM/src/com_crm_pom/Products.java
@@ -1,5 +1,22 @@
 package com_crm_pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Products {
+@FindBy(xpath="//input[@value='New Product']")
+private WebElement newproductbtn;
+@FindBy(name="property(Product Code)")
+private WebElement prdcode;
+@FindBy(name="property(Product Category)")
+private WebElement prdcategeory;
+@FindBy(xpath="//input[@name='property(Sales End Date)']")
+private WebElement salesenddata;
+@FindBy(xpath="//input[@name= 'property(Support Expiry Date)']")
+private WebElement supportexpiry;
+@FindBy(xpath="//input[@name='property(Product Name)']")
+private WebElement prodname;
+@FindBy(xpath = "//input[@name='property(Vendor Name)']")
+private WebElement vendorname;
 
 }
